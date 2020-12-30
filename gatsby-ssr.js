1f7a2d0d643fe30a9e8412dc33aa13e76a978a50
Niklas Dahlqvist <dalkmania@gmail.com>
2020-12-30 10:36:55 -0800

    Initial Commit

---
+++ gatsby-ssr.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+const getLanguage = (pathname, pluginOptions) => {
+  const currentLocale = pathname.match(/^\/([^?\/]+)/)
+  const locales = Object.keys(pluginOptions.locales)
+
+  if((currentLocale !== null) && locales.filter(locale => locale == currentLocale)) {
+    return pluginOptions.locales[currentLocale[1]]
+  } else {
+    return pluginOptions.locales[pluginOptions.defaultLocale]
+  }
+}
+
+export const onRenderBody = ({ pathname, setHeadComponents }, pluginOptions) => {
+  if (
+    process.env.NODE_ENV === `production` ||
+    pluginOptions.includeInDevelopment
+  ) {
+    const id = getLanguage(pathname, pluginOptions)
+
+    return setHeadComponents([
+      <script
+        key={`gatsby-plugin-snap-engage`}
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function() {
+            var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+            se.src = '//storage.googleapis.com/code.snapengage.com/js/${id}.js';
+            var done = false;
+            se.onload = se.onreadystatechange = function() {
+              if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+              }
+            };
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+          })();
+          `,
+        }}
+      />,
+    ])
+  }
+}
