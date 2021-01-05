e726079e6254fc884c7d243f2097c90c88a9992a
Niklas Dahlqvist <dalkmania@gmail.com>
2021-01-05 10:40:26 -0800

    Added Unit Tests

---
+++ __tests__/gatsby-ssr.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _gatsbySsr = require("../gatsby-ssr");
+
+describe("gatsby-plugin-snap-engage", function () {
+  describe("onRenderBody", function () {
+    describe("in development mode", function () {
+      it("it does not replace any head components, if the includeInDevelopment option is false", function () {});
+    });
+    describe("in production mode", function () {});
+  });
+});
---
+++ src/__tests__/gatsby-ssr.js
@@ -0,0 +1,16 @@
+import { onRenderBody } from "../gatsby-ssr"
+
+describe(`gatsby-plugin-snap-engage`, () => {
+    describe(`onRenderBody`, () => {
+        describe(`in development mode`, () => {
+            it(`it does not replace any head components, if the includeInDevelopment option is false`, () => {
+                
+            })
+
+        })
+
+        describe(`in production mode`, () => {
+            
+        })
+    })
+})
