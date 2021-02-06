1a66e0e7756cfc0d59e041dfab19ac10cb1f3b6a
Niklas Dahlqvist <dalkmania@gmail.com>
2021-02-06 07:53:01 -0800

    Replaced window.reload()

--- src/__tests__/gatsby-browser.js
+++ src/__tests__/gatsby-browser.js
@@ -1,169 +1,168 @@
-import { onPreRouteUpdate } from "../gatsby-browser"
+import { onPreRouteUpdate } from "../gatsby-browser";
 
 describe(`gatsby-plugin-snap-engage`, () => {
     describe(`onPreRouteUpdate`, () => {
         describe(`in development mode`, () => {
-            const { reload } = window.location;
+            const { assign } = window.location;
 
             beforeAll(() => {
-                Object.defineProperty(window, 'location', {
+                Object.defineProperty(window, "location", {
                     writable: true,
-                    value: { reload: jest.fn() },
-                })
-            })
+                    value: { assign: jest.fn() }
+                });
+            });
 
             afterEach(() => {
                 jest.clearAllMocks();
-            })
+            });
 
             afterAll(() => {
-                window.location.reload = reload
-            })
-            
-            it(`it does not call any functions, if the includeInDevelopment and multilingual options are false`, () => {
-                const pluginOptions = { multilingual: false, id: '1', includeInDevelopment: false }
-                const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
-                const location = {
-                    pathname: '/de/some-slug'
-                }
-                const getLanguage = jest.fn()
+                window.location.assign = assign;
+            });
 
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(getLanguage).not.toHaveBeenCalled()
-            })
+            it(`it does not call any functions, if the includeInDevelopment and multilingual options are false`, () => {
+                const pluginOptions = { multilingual: false, id: "1", includeInDevelopment: false };
+                const prevLocation = {
+                    pathname: "/fr/some-slug"
+                };
+                const location = {
+                    pathname: "/de/some-slug"
+                };
+                const getLanguage = jest.fn();
+
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(getLanguage).not.toHaveBeenCalled();
+            });
 
             it(`it does call functions, if the includeInDevelopment and multilingual options are true, and the language slug is changed`, () => {
-                const pluginOptions = { 
-                    multilingual: true, 
-                    id: '1',
-                    defaultLocale: 'en',
+                const pluginOptions = {
+                    multilingual: true,
+                    id: "1",
+                    defaultLocale: "en",
                     locales: {
-                    'en': 'english-script',
-                    'fr': 'french-script'
-                    }, 
-                    includeInDevelopment: true 
-                }
+                        en: "english-script",
+                        fr: "french-script"
+                    },
+                    includeInDevelopment: true
+                };
                 const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
+                    pathname: "/fr/some-slug"
+                };
                 const location = {
-                    pathname: '/de/some-slug'
-                }
+                    pathname: "/de/some-slug"
+                };
 
-                expect(jest.isMockFunction(window.location.reload)).toBe(true)
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(window.location.reload).toHaveBeenCalled()
-            })
+                expect(jest.isMockFunction(window.location.assign)).toBe(true);
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(window.location.assign).toHaveBeenCalled();
+            });
 
             it(`it does not call functions, if the language slug is not changed`, () => {
-                const pluginOptions = { 
-                    multilingual: true, 
-                    id: '1',
-                    defaultLocale: 'en',
+                const pluginOptions = {
+                    multilingual: true,
+                    id: "1",
+                    defaultLocale: "en",
                     locales: {
-                    'en': 'english-script',
-                    'fr': 'french-script'
-                    }, 
-                    includeInDevelopment: true 
-                }
+                        en: "english-script",
+                        fr: "french-script"
+                    },
+                    includeInDevelopment: true
+                };
                 const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
+                    pathname: "/fr/some-slug"
+                };
                 const location = {
-                    pathname: '/fr/some-other-slug'
-                }
-                jest.restoreAllMocks()
-                expect(jest.isMockFunction(window.location.reload)).toBe(true)
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(window.location.reload).not.toHaveBeenCalled()
-            })
-        })
+                    pathname: "/fr/some-other-slug"
+                };
+                jest.restoreAllMocks();
+                expect(jest.isMockFunction(window.location.assign)).toBe(true);
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(window.location.assign).not.toHaveBeenCalled();
+            });
+        });
 
         describe(`in production mode`, () => {
-            const { reload } = window.location
-            let env
+            const { reload } = window.location;
+            let env;
 
             beforeAll(() => {
-                env = process.env.NODE_ENV
-                process.env.NODE_ENV = `production`
+                env = process.env.NODE_ENV;
+                process.env.NODE_ENV = `production`;
 
-                Object.defineProperty(window, 'location', {
+                Object.defineProperty(window, "location", {
                     writable: true,
-                    value: { reload: jest.fn() },
-                })
-            })
+                    value: { assign: jest.fn() }
+                });
+            });
 
             afterEach(() => {
                 jest.clearAllMocks();
-            })
+            });
 
             afterAll(() => {
-                process.env.NODE_ENV = env
-                window.location.reload = reload
-            })
+                process.env.NODE_ENV = env;
+                window.location.assign = assign;
+            });
 
             it(`it does not call any functions, if the multilingual option is false`, () => {
-                const pluginOptions = { multilingual: false, id: '1', includeInDevelopment: false }
+                const pluginOptions = { multilingual: false, id: "1", includeInDevelopment: false };
                 const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
+                    pathname: "/fr/some-slug"
+                };
                 const location = {
-                    pathname: '/de/some-slug'
-                }
-                const getLanguage = jest.fn()
+                    pathname: "/de/some-slug"
+                };
+                const getLanguage = jest.fn();
 
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(getLanguage).not.toHaveBeenCalled()
-            })
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(getLanguage).not.toHaveBeenCalled();
+            });
 
             it(`it does call functions, if the language slug is changed`, () => {
-                const pluginOptions = { 
-                    multilingual: true, 
-                    id: '1',
-                    defaultLocale: 'en',
+                const pluginOptions = {
+                    multilingual: true,
+                    id: "1",
+                    defaultLocale: "en",
                     locales: {
-                    'en': 'english-script',
-                    'fr': 'french-script'
-                    }, 
-                    includeInDevelopment: false 
-                }
+                        en: "english-script",
+                        fr: "french-script"
+                    },
+                    includeInDevelopment: false
+                };
                 const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
+                    pathname: "/fr/some-slug"
+                };
                 const location = {
-                    pathname: '/de/some-slug'
-                }
+                    pathname: "/de/some-slug"
+                };
 
-                expect(jest.isMockFunction(window.location.reload)).toBe(true)
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(window.location.reload).toHaveBeenCalled()
-            })
+                expect(jest.isMockFunction(window.location.assign)).toBe(true);
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(window.location.assign).toHaveBeenCalled();
+            });
 
             it(`it does not call functions, if the language slug is not changed`, () => {
-                const pluginOptions = { 
-                    multilingual: true, 
-                    id: '1',
-                    defaultLocale: 'en',
+                const pluginOptions = {
+                    multilingual: true,
+                    id: "1",
+                    defaultLocale: "en",
                     locales: {
-                    'en': 'english-script',
-                    'fr': 'french-script'
-                    }, 
-                    includeInDevelopment: true 
-                }
+                        en: "english-script",
+                        fr: "french-script"
+                    },
+                    includeInDevelopment: true
+                };
                 const prevLocation = {
-                    pathname: '/fr/some-slug'
-                }
+                    pathname: "/fr/some-slug"
+                };
                 const location = {
-                    pathname: '/fr/some-other-slug'
-                }
-                jest.restoreAllMocks()
-                expect(jest.isMockFunction(window.location.reload)).toBe(true)
-                onPreRouteUpdate({ prevLocation, location }, pluginOptions)
-                expect(window.location.reload).not.toHaveBeenCalled()
-            })
-
-        })
-    })
-})
+                    pathname: "/fr/some-other-slug"
+                };
+                jest.restoreAllMocks();
+                expect(jest.isMockFunction(window.location.assign)).toBe(true);
+                onPreRouteUpdate({ prevLocation, location }, pluginOptions);
+                expect(window.location.assign).not.toHaveBeenCalled();
+            });
+        });
+    });
+});
--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,15 +1,17 @@
-import { getLanguage } from './helpers/getLanguage'
+import { getLanguage } from "./helpers/getLanguage";
 
 export const onPreRouteUpdate = ({ prevLocation, location }, pluginOptions) => {
-  if(pluginOptions.multilingual === true && (process.env.NODE_ENV === `production` || pluginOptions.includeInDevelopment)) {
-    if(prevLocation !== null) {
-      const prevId = getLanguage(prevLocation.pathname, pluginOptions)
-      const currId = getLanguage(location.pathname, pluginOptions)
+    if (
+        pluginOptions.multilingual === true &&
+        (process.env.NODE_ENV === `production` || pluginOptions.includeInDevelopment)
+    ) {
+        if (prevLocation) {
+            const prevId = getLanguage(prevLocation.pathname, pluginOptions);
+            const currId = getLanguage(location.pathname, pluginOptions);
 
-      if( prevId !== currId) {
-          window.location.reload()
-      }
+            if (prevId !== currId) {
+                window.location.assign(window.location.href);
+            }
+        }
     }
-            
-  }
-}
+};
